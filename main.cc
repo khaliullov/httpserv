6be0c9ae18bb925ed1d2bc3e51ed2ad28c02358b
John Schmerge <jbschmerge@gmail.com>
2014-11-07 13:45:00 -0500

    Code now listens on multiple sockets (woohoo)

--- main.cc
+++ main.cc
@@ -1,22 +1,24 @@
-#include <cstdio>
 #include <unistd.h>
 #include <sys/socket.h>
-#include <utility/util.h>
+#include <cstdio>
+#include <vector>
 
+#include "utility/util.h"
 #include "serverconfig.h"
 
 int main(int argc, char ** argv)
 {
 	server_config config(argc, argv);
+	std::vector<int> serverFds;
 
 	for (auto & addr : config.server_address_list)
 	{
-		addr.print();
+		int sfd = socket(addr.get_addr()->sa_family, SOCK_STREAM, 0);
 
-		printf("--> family = %d\n", addr.addr()->sa_family);
-		int sfd = socket(addr.addr()->sa_family, SOCK_STREAM, 0);
+		printf("Server listening on address %s\n", addr.str().c_str());
 
-		if (addr.addr()->sa_family == 10)
+		if (  addr.get_addr()->sa_family == 10
+		   && config.server_address_list.size() != 1)
 		{
 			int x = 1;
 			setsockopt(sfd, IPPROTO_IPV6, IPV6_V6ONLY, &x, sizeof(x));
@@ -25,14 +27,14 @@ int main(int argc, char ** argv)
 		if (sfd < 0)
 			throw make_syserr("socket() failed");
 
-		if (bind(sfd, addr.addr(), addr.addrlen()) != 0)
+		if (bind(sfd, addr.get_addr(), addr.get_addrlen()) != 0)
 			throw make_syserr("bind() failed");
-
 
 		if (listen(sfd, 5) != 0)
 			throw make_syserr("listen() failed");
+
+		serverFds.push_back(sfd);
 	}
-	sleep(120);
 
 	return 0;
 }
