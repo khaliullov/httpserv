f7fa77df2ecba8064588f31dacc9cb630e38029d
John Schmerge <jbschmerge@gmail.com>
2014-11-07 14:37:32 -0500

    Commiting changes before moving to header

--- tcp_server_socket.cc
+++ tcp_server_socket.cc
@@ -12,118 +12,53 @@
 #include <memory>
 #include <string>
 
+#include "utility/util.h"
+#include "address.h"
 #include "net_error.h"
 
 namespace net {
 
-
-class address
+//////////////////////////////////////////////////////////////////////
+struct socket_option
 {
-	struct FreeInfoFunctor {
-		void operator () (struct addrinfo * p) { freeaddrinfo(p); }
-	};
-
-	typedef std::unique_ptr<struct addrinfo, FreeInfoFunctor> address_list;
-
  public:
-	address(const std::string & address, const std::string & service)
-	  : m_sockaddrlen(0)
-	  , m_sockaddr()
-	  , m_address(address)
-	  , m_service(service)
-	{ }
-
-	address(const sockaddr * addr, socklen_t addrlen)
-	  : m_sockaddrlen(addrlen)
-	  , m_sockaddr(m_sockaddrlen ? new char[m_sockaddrlen] : nullptr)
-	  , m_address()
-	  , m_service()
+	socket_option(int _level, int _optname, void * _optval, socklen_t _len)
+	  : level(_level)
+	  , optname(_optname)
+	  , length(_len)
+	  , option_content(new char[length])
 	{
-		memcpy(m_sockaddr.get(), addr, addrlen);
+		memcpy(option_content.get(), _optval, length);
 	}
 
-	address(const address & other)
-	  : m_sockaddrlen(other.m_sockaddrlen)
-	  , m_sockaddr(m_sockaddrlen ? new char[m_sockaddrlen] : nullptr)
-	  , m_address(other.m_address)
-	  , m_service(other.m_service)
+	~socket_option() { }
+
+	void set(int fd) const
 	{
-		memcpy(m_sockaddr.get(), other.m_sockaddr.get(), m_sockaddrlen);
+		if (setsockopt(fd, level, optname, option_content.get(), length) != 0)
+			throw make_syserr("Could not set socket option");
 	}
 
-	~address()
-	{ }
-
-	void resolve_sock_addr(int flags = (NI_NUMERICSERV))
-	{
-		const int buflen = 80;
-		char buffer1[80], buffer2[80];
-
-		int rc = getnameinfo(reinterpret_cast<struct sockaddr*>(m_sockaddr.get()),
-		                     m_sockaddrlen, buffer1, buflen,
-		                     buffer2, buflen, flags);
-
-		if (rc) throw net::make_ainfo_error(rc, __func__);
-		m_address = buffer1;
-		m_service = buffer2;
-	}
-
-	const sockaddr * addr() const
-	{ return reinterpret_cast<struct sockaddr*>(m_sockaddr.get()); }
-
-	socklen_t addrlen() const
-	{ return m_sockaddrlen; }
-
-	void print()
-	{
-		if (m_address.empty() || m_service.empty())
-			resolve_sock_addr();
-
-		printf("%s:%s\n", m_address.c_str(), m_service.c_str());
-	}
-
-	static std::vector<address>
-	list_passive_addresses(const int type,
-	                       const std::string & portNumber)
-	{
-		std::vector<address> retlist;
-		int rc = 0;
-
-		struct addrinfo * tmp = nullptr;
-		struct addrinfo hints;
-		memset(&hints, 0, sizeof(hints));
-		hints.ai_family = AF_UNSPEC;
-		hints.ai_socktype = type;
-		hints.ai_flags = AI_PASSIVE;
-
-		if ((rc = getaddrinfo(nullptr, portNumber.c_str(), &hints, &tmp)) != 0)
-			throw net::make_ainfo_error(rc, __func__);
-
-		address_list info(tmp);
-
-		for (tmp = info.get(); tmp != nullptr; tmp = tmp->ai_next)
-		{
-			retlist.emplace_back(tmp->ai_addr, tmp->ai_addrlen);
-		}
-		return retlist;
-	}
-
- protected:
-	socklen_t m_sockaddrlen;
-	std::unique_ptr<char[]> m_sockaddr;
-	std::string m_address;
-	std::string m_service;
+ private:
+	int level;
+	int optname;
+	socklen_t length;
+	std::unique_ptr<char[]> option_content;
 };
+
+//////////////////////////////////////////////////////////////////////
+typedef std::vector<socket_option> socket_option_list;
 
 //////////////////////////////////////////////////////////////////////
 class socket
 {
  public:
-	socket(int domain, int type, int protocol)
-	: fd(::socket(domain, type, protocol))
-	{
-		if (fd < 0) throw std::runtime_error("Could not create socket");
-	}
+	socket(int domain, int type, int protocol = 0)
+	  : fd(::socket(domain, type, protocol))
+		{ if (fd < 0) throw std::runtime_error("Could not create socket"); }
+
+	void set_options(const socket_option_list & list)
+		{ for (const auto & o : list) o.set(fd); }
 
 	virtual ~socket() { close(fd); }
 
@@ -135,9 +70,15 @@ class socket
 class tcp_server_socket : net::socket {
  public:
 	tcp_server_socket(const address & a)
-	: socket(AF_INET, SOCK_STREAM, 0)
-	, m_address(a)
+	  : socket(a.get_addr()->sa_family, SOCK_STREAM)
+	  , m_address(a)
 	{
+	}
+
+	tcp_server_socket(const address & a, const socket_option_list & opts)
+	  : tcp_server_socket(a)
+	{
+		this->set_options(opts);
 	}
 
  private:
@@ -145,19 +86,3 @@ class tcp_server_socket : net::socket {
 };
 
 } // namespace net
-
-//////////////////////////////////////////////////////////////////////
-int main(int argc, char ** argv)
-{
-	for (int i = 1; i < argc; ++i)
-	{
-		printf("%s\n", argv[i]);
-		auto list =
-			net::address::list_passive_addresses(SOCK_STREAM, argv[i]);
-
-		for (auto i : list)
-		{
-			i.print();
-		}
-	}
-}
