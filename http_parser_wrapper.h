694933282d26a16caff5c4f336b388a615af2704
John Schmerge <john.schmerge@gmail.com>
2014-11-09 18:24:12 -0500

    Fleshing this out

--- http_parser_wrapper.h
+++ http_parser_wrapper.h
@@ -1,11 +1,43 @@
 #ifndef GUARD_HTTP_PARSER_WRAPPER
 #define GUARD_HTTP_PARSER_WRAPPER 1
 
-namespace protocols {
+#include <string>
 
-class http_parser {
+class http_server
+{
+
+	virtual void message_start()
+	{ printf("Start of message\n"); }
+
+	virtual void process_url(const char * data, size_t length)
+	{
+		std::string s(data, length);
+		printf("%s: data = %s\n", __func__, s.c_str());
+	}
+
+	virtual void header_name(const char * data, size_t length) 
+	{
+		std::string s(data, length);
+		printf("%s: data = %s\n", __func__, s.c_str());
+	}
+
+	virtual void header_value(const char * data, size_t length) 
+	{
+		std::string s(data, length);
+		printf("%s: data = %s\n", __func__, s.c_str());
+	}
+
+	virtual void headers_complete() 
+	{ printf("End of headers\n"); }
+
+	virtual void body_data(const char * data, size_t length) 
+	{
+		std::string s(data, length);
+		printf("%s: data = %s\n", __func__, s.c_str());
+	}
+
+	virtual void message_complete() 
+	{ printf("End of message\n"); }
 };
-
-} // namespace protocols
 
 #endif // GUARD_HTTP_PARSER_WRAPPER
