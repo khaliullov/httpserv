473c56e58d6ededbf9546091277e8b245c9b85fd
John Schmerge <jbschmerge@gmail.com>
2014-11-18 15:02:24 -0500

    Cleanup

--- net_error.h
+++ net_error.h
@@ -1,33 +1,9 @@
 #ifndef GUARD_NET_ERROR_H
 #define GUARD_NET_ERROR_H 1
 
-#include <netdb.h>
-
 #include <system_error>
 
 namespace net {
-
-//////////////////////////////////////////////////////////////////////
-#if 0
-class ainfo_error_category : public std::error_category
-{
- public:
-	ainfo_error_category() noexcept : std::error_category() { }
-
-	~ainfo_error_category() { };
-
-	const char * name() const noexcept override
-	{ return "addrinfo"; }
-
-	std::string message(int cond) const override
-	{ return gai_strerror(cond); }
-
-	ainfo_error_category(const ainfo_error_category &) = delete;
-
-	ainfo_error_category &
-	operator = (const ainfo_error_category&) = delete;
-};
-#endif
 
 //////////////////////////////////////////////////////////////////////
 const std::error_category & addrinfo_category();
